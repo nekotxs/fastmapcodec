e5c63a2abfda2297f1991aed199fd8e43cb924da
neko <68693406+nekotxs@users.noreply.github.com>
2026-08-06 00:23:08 +0700

    log4->slf4j

--- fabric/src/main/java/dev/neko/fastmapcodec/FastMapCodecFabric.java
+++ fabric/src/main/java/dev/neko/fastmapcodec/FastMapCodecFabric.java
@@ -1,12 +1,12 @@
 package dev.neko.fastmapcodec;
 
 import net.fabricmc.api.ModInitializer;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class FastMapCodecFabric implements ModInitializer {
     static String MOD_ID = "fastmapcodec";
-    static Logger logger = LogManager.getLogger(MOD_ID);
+    static Logger logger = LoggerFactory.getLogger(MOD_ID);
 
     @Override
     public void onInitialize() {
--- neoforge/src/mod/java/dev/neko/fastmapcodec/neoforge/FastMapCodecNeoForge.java
+++ neoforge/src/mod/java/dev/neko/fastmapcodec/neoforge/FastMapCodecNeoForge.java
@@ -4,13 +4,13 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 
 import net.neoforged.fml.javafmlmod.FMLModContainer;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 
 @Mod("fastmapcodec")
 public class FastMapCodecNeoForge {
     static String MOD_ID = "fastmapcodec";
-    static Logger logger = LogManager.getLogger(MOD_ID);
+    static Logger logger = LoggerFactory.getLogger(MOD_ID);
 
     public FastMapCodecNeoForge(FMLModContainer container, IEventBus modBus) {
         logger.info("FastMapCodec mod initialized");
